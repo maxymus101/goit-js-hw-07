944463ddff68866fbc2b94443d36d655dd8802b0
Max Akimov <maxymus2828@gmail.com>
2025-02-17 02:54:37 +0200

    done task-5 and task-6

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const color = document.querySelector(".color");
+const changeColor = document.querySelector(".change-color");
+changeColor.addEventListener("click", (event) => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  console.log("randomColor :>> ", randomColor);
+  color.textContent = randomColor;
+  console.log("color value :>> ", color.textContent);
+});
